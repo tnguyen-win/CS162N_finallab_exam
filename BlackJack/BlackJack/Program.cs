3726634a70c3305d4b4c542978f0cddfa5f7f897
tnguyen <129979622+tnguyen-win@users.noreply.github.com>
2023-06-14 13:28:04 -0700

    Final - P2 + P3

---
+++ BlackJack/BlackJack/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using CardClasses;
+
+namespace BlackJack {
+	class Program {
+		static void Program1() {
+			Random r = new();
+			BJHand b = new();
+
+			for (var i = 0; i < 7; i++) {
+				Card c = new(r.Next(1, 4), r.Next(1, 13));
+
+				b.AddCard(c);
+			}
+
+			Console.WriteLine("[PROBLEM #2]\n");
+			Console.WriteLine($"BEFORE SORTING BY SCORE - [\n{b}]\n");
+
+			b.Sort();
+
+			Console.WriteLine($"AFTER SORTING BY SCORE - [\n{b}]\n");
+		}
+
+		static void Program2() {
+			Deck d = new();
+			BJHand b = new(d, 7);
+
+			Console.WriteLine("[PROBLEM #3]\n");
+			Console.WriteLine("GETTING CARDS IN LIST WITH FOREACH LOOP - [");
+			foreach (Card c in b) Console.WriteLine(c);
+			Console.WriteLine("]");
+		}
+
+		static void Main() {
+			Program1();
+			Program2();
+		}
+	}
+}
